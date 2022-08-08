fa7ed3028cc96b866a52fe624a9e5827eb609b89
Matt Dorchak <matthewdorchak235@gmail.com>
2022-08-08 18:21:19 -0400

    Create basic site structure and pseudocode

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// Find which row is next in line
+
+
+// When a button is pressed, place the text of the button in 
+// the next available box
+
+        // Add a backspace button to remove the most recent input
+
+// When a test button is selected, check the order of the inputs
+        // If the input is in the exact spot, add a green background
+        
+        // If the input is in the order but not the right spot,
+        // add a yellow background
+
+// When the correct order is inputted, disable the buttons
+
+// Share button that copies the results into a simple sharable box
